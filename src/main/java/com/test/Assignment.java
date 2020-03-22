1781c6e5c56338731f5108bed49c7ca045501b97
Shridhar Chippa <15193271+shridhaarchippaa@users.noreply.github.com>
2020-03-22 18:50:30 +0000

    Create Assignment.java

---
+++ src/main/java/com/test/Assignment.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment.class, args);
+	}
+}
